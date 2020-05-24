df713a299f179a9a8ef779fabdbed8a48fe41bb9
vietdung0607 <vietdungst@gmail.com>
2020-05-24 16:23:03 +0700

    Bài 25: API Quản lý action cho function

---
+++ src/KnowledgeSpace.BackendServer/Controllers/CommandsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KnowledgeSpace.BackendServer.Data;
+using KnowledgeSpace.BackendServer.Data.Entities;
+using KnowledgeSpace.ViewModels.Systems;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KnowledgeSpace.BackendServer.Controllers
+{
+    public class CommandsController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+        public CommandsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // URL: GET
+        [HttpGet]
+        public async Task<IActionResult> GetCommands()
+        {
+            var commands = _context.Commands;
+
+            var commandVms = await commands.Select(u => new CommandVm()
+            {
+                Id = u.Id,
+                Name = u.Name
+            }).ToListAsync();
+
+            return Ok(commandVms);
+        }
+        
+    }
+}
