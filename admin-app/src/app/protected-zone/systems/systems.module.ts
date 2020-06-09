02845692f26cbcf93b4fc261ec93b770d568077c
vietdung0607 <vietdungst@gmail.com>
2020-06-09 22:11:32 +0700

    Create form Roles in Systems folder

--- admin-app/src/app/protected-zone/systems/systems.module.ts
+++ admin-app/src/app/protected-zone/systems/systems.module.ts
@@ -5,12 +5,37 @@ import { UsersComponent } from './users/users.component';
 import { RolesComponent } from './roles/roles.component';
 import { PermissionsComponent } from './permissions/permissions.component';
 import { SystemsRoutingModule } from './systems-routing.module';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
+import { BlockUIModule } from 'primeng/blockui';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { RolesDetailComponent } from './roles/roles-detail/roles-detail.component';
+import { NotificationService } from '@app/shared/services';
+import { BsModalService, ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ValidationMessageModule } from '@app/shared/modules/validation-message/validation-message.module';
 
 @NgModule({
-  declarations: [FunctionsComponent, UsersComponent, RolesComponent, PermissionsComponent],
+  declarations: [FunctionsComponent, UsersComponent, RolesComponent, PermissionsComponent, RolesDetailComponent],
   imports: [
     CommonModule,
-    SystemsRoutingModule
+    SystemsRoutingModule,
+    PanelModule,
+    ButtonModule,
+    TableModule,
+    PaginatorModule,
+    BlockUIModule,
+    ProgressSpinnerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ValidationMessageModule,
+    ModalModule.forRoot()
+  ],
+  providers: [
+    NotificationService,
+    BsModalService,
   ]
 })
 export class SystemsModule { }
--- admin-app/src/app/shared/services/index.ts
+++ admin-app/src/app/shared/services/index.ts
@@ -2,3 +2,5 @@ export * from './auth.service';
 export * from './base.service';
 export * from './users.service';
 export * from './utilities.service';
+export * from './roles.service';
+export * from './notifications.service';
