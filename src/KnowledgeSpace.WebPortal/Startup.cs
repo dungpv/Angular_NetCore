0b85637eb4628c411a8be5296d2011de2a0a3303
vietdung0607 <vietdungst@gmail.com>
2020-07-28 17:18:42 +0700

    Custom Capcha code web portal

--- src/KnowledgeSpace.WebPortal/Startup.cs
+++ src/KnowledgeSpace.WebPortal/Startup.cs
@@ -193,6 +193,7 @@ namespace KnowledgeSpace.WebPortal
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
